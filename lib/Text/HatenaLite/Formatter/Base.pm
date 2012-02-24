5682d80c536d207c81460eb3ac5dbe0bc1ba2232
wakabatan <wakabatan@localhost.localdomain>
2012-02-24 19:31:57 +0900

    Split base class

---
+++ lib/Text/HatenaLite/Formatter/Base.pm
@@ -0,0 +1,18 @@
+package Text::HatenaLite::Formatter::Base;
+use strict;
+use warnings;
+our $VERSION = '1.0';
+
+sub new {
+    my $class = shift;
+    return bless {}, $class;
+}
+
+sub parsed_data {
+    if (@_ > 1) {
+        $_[0]->{parsed_data} = $_[1];
+    }
+    return $_[0]->{parsed_data};
+}
+
+1;
--- lib/Text/HatenaLite/Formatter/PlainText.pm
+++ lib/Text/HatenaLite/Formatter/PlainText.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 our $VERSION = '1.0';
 use Text::HatenaLite::Definitions;
+use Text::HatenaLite::Formatter::Base;
+push our @ISA, qw(Text::HatenaLite::Formatter::Base);
 
 my $Notations = {};
 for my $def (@$Text::HatenaLite::Definitions::Notations) {
@@ -10,18 +12,6 @@ for my $def (@$Text::HatenaLite::Definitions::Notations) {
 }
 
 $Notations->{text} = $Text::HatenaLite::Definitions::TextNotation;
-
-sub new {
-    my $class = shift;
-    return bless {}, $class;
-}
-
-sub parsed_data {
-    if (@_ > 1) {
-        $_[0]->{parsed_data} = $_[1];
-    }
-    return $_[0]->{parsed_data};
-}
 
 sub as_text {
     my $self = shift;
